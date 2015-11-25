70d298c497dd7820dd277cf7403219dc9831c245
rjain236 <raja.jain@finmechanics.com>
2015-11-25 11:53:26 +0800

    adding aparapi

---
+++ aparapi/samples/add/src/com/amd/aparapi/sample/add/Test.java
@@ -0,0 +1,17 @@
+package com.amd.aparapi.sample.add;
+
+/**
+ * Created by rjain236 on 25/11/15.
+ */
+public final class Test {
+
+    float x;
+
+    public Test(float x) {
+        this.x = x;
+    }
+
+    public float getX() {
+        return x;
+    }
+}
