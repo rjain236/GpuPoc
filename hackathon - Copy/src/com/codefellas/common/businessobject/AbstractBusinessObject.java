ba259723afb47f8413d8835872545ca30062ea54
ankita.sharma <ankita.sharma@finmechanics.com>
2015-11-27 11:20:17 +0800

    rem copy

--- hackathon - Copy/src/com/codefellas/common/businessobject/AbstractBusinessObject.java
+++
@@ -1,7 +0,0 @@
-package com.codefellas.common.businessobject;
-
-/**
- * Created by rjain236 on 25/11/15.
- */
-public class AbstractBusinessObject implements BusinessObject{
-}
--- hackathon - Copy/src/com/codefellas/common/commonobjects/CommonObjects.java
+++
@@ -1,7 +0,0 @@
-package com.codefellas.common.commonobjects;
-
-/**
- * Created by rjain236 on 25/11/15.
- */
-public interface CommonObjects {
-}
